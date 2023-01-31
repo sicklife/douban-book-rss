2fdb87e1d9e9d6ed53af0b1e2c73fefa5c81e4dd
李际朝 <jizhao.li@istarshine.com>
2023-02-01 07:08:18 +0800

    增加了定时刷新功能。主要功能已经完成。

--- background.js
+++ background.js
@@ -31,13 +31,35 @@ chrome.webNavigation.onCompleted.addListener(() => {
     console.info("你好豆瓣书评~");
 }, filter);
 
+let book_review_list = []
+
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse){
+    let review_msg = message.text;
+    let review_msg_list = review_msg.split("\n");
+    let new_review_lit = [];
+    for (let i = 0; i < review_msg_list.length; i++) {
+        let line = review_msg_list[i];
+        if(book_review_list.includes(line)){
+        }else{
+            book_review_list.push(line)
+            new_review_lit.push(line)
+        }
+    }
+    book_review_list = book_review_list.slice(-20);
 
-    postData('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=d31e5a41-45bd-4ae6-910e-6c0f46905f6e',
-        { "msgtype": "text", "text": {"content": message.text} })
-        .then(data => {
-            console.log(data); // JSON data parsed by `data.json()` call
-        });
+    let wx_msg;
+    if(new_review_lit.length>0){
+        wx_msg = new_review_lit.join("\n");
+        postData('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=d31e5a41-45bd-4ae6-910e-6c0f46905f6e',
+            // { "msgtype": "text", "text": {"content": message.text} })
+            { "msgtype": "text", "text": {"content": wx_msg} })
+            .then(data => {
+                console.log(data); // JSON data parsed by `data.json()` call
+            });
+    }else {
+        wx_msg = "没有新书评."
+        console.log(wx_msg)
+    }
     sendResponse("收到！");
 })
